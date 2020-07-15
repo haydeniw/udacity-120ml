6bb457b91b55fb24f0b26990ce41f16dcb599649
Hayden Wolff <haydenwolff99@gmail.com>
2020-07-14 22:49:25 -0400

    finished naive bayesian lab

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -15,12 +15,25 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+classifier = GaussianNB()
+t0_fit = time()
+classifier.fit(features_train, labels_train)
+print("Training time: ", round((time() - t0_fit), 3), "s")
+t0_train = time()
+prediction = classifier.predict(features_test)
+print("Prediction time: ", round(time() - t0_train, 3), "s")
+accuracy = accuracy_score(prediction, labels_test)
+print("Accuracy is: ", accuracy)
+
+
 
 
 
